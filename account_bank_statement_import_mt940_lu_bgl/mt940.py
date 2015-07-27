aa435e947abb8c9984c2b80385c09f61910c5097
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2015-07-27 16:38:21 +0200

    [PEP8] little temporay fix

--- account_bank_statement_import_mt940_lu_bgl/mt940.py
+++ account_bank_statement_import_mt940_lu_bgl/mt940.py
@@ -24,7 +24,7 @@ import re
 import logging
 
 from openerp.addons.account_bank_statement_import_mt940_base.mt940 import (
-    MT940, str2amount, get_subfields, handle_common_subfields)
+    MT940, str2amount)  # , get_subfields, handle_common_subfields)
 
 _logger = logging.getLogger(__name__)
 
